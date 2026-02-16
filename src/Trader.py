ac2145e0b2e1f2872e9c6eb88c9f5442cd4b8aa2
funnymonke0 <aivectorec@gmail.com>
2026-02-16 16:31:59 -0500

    much better accuracy model + i did stuff that I for sure need to verify tmw at market open

--- src/Trader.py
+++ src/Trader.py
@@ -1,21 +1,82 @@
-from alpaca.trading.client import TradingClient
 from pathlib import Path
+from math import log
+import json
+from alpaca.data.live import StockDataStream
+from alpaca.trading.client import TradingClient
 import torch
 from StockModel import StockModel
-API_KEY = ""
-SECRET_KEY = ""
-PATH_TO_DATASETS = Path(r"datasets\5_us_txt\data\5 min\us")
+
+PATH_TO_DATASETS = r"datasets\5_us_txt\data\5 min\us"
+
 PATH_TO_MODELS = Path(r"models")
-MODEL_NAME = Path(r"model5.pth")
+PATH_TO_KEYS = Path(r"config")
+API_KEY_FILE = PATH_TO_KEYS / r"public_key.txt"
+SECRET_KEY_FILE = PATH_TO_KEYS / r"secret_key.txt"
+MODEL_NAME = r"model1_weights.pth"
+PATH_TO_PRECOMPUTE = Path(r"precompute_cache")
+TICKERS = ["AAPL", "MSFT", "GOOGL", "AMZN", "META", "TSLA", "NVDA", "JPM", "V", "UNH"]
+
+
 class Trader():
     def __init__(self):
-        self.trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
-        self.model = StockModel(hidden_layers=[64,64,32], embedding_dims=8)
+        self.prev_close = None
+        self.embedding_map = {}
+        with open(PATH_TO_PRECOMPUTE/'embedding_lookup.json', 'r') as f:
+            self.embedding_map = json.load(f)
 
-    def load_model(self):
-        state_dict = torch.load(Path(PATH_TO_MODELS, MODEL_NAME))
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        x_id_tensor = torch.load(PATH_TO_PRECOMPUTE / r"x_id_tensor.pt")
+        embed_size = x_id_tensor.max().item() + 1
+        self.model = StockModel(feature_size=5, embed_size=embed_size)
+        state_dict = torch.load(PATH_TO_MODELS / MODEL_NAME, map_location=self.device,weights_only=True)
         self.model.load_state_dict(state_dict)
-    
-    def signal_generator(self):
+        self.model.to(self.device)
+        self.model = torch.compile(self.model)
         self.model.eval()
+
+        with open(API_KEY_FILE, "r") as f:
+            API_KEY = f.readline()
+        with open(SECRET_KEY_FILE, "r") as f:
+            SECRET_KEY = f.readline()
+        self.stream = StockDataStream(API_KEY, SECRET_KEY)
+        self.trading_client = TradingClient(API_KEY, SECRET_KEY)
+
+    
+    def stream_data(self):
+        # Connect to the Alpaca data stream and subscribe to the desired tickers
+        print("Connecting to Alpaca data stream...")
+        self.stream.subscribe_bars(self.preprocess, *TICKERS, )
+        print("Subscribed to tickers: ", TICKERS)
+        self.stream.run()
+
+
+    def signal_generator(self, symbol, features):
+        ticker_id = self.embedding_map[symbol+".US"]
+        x_idx = torch.tensor([ticker_id], dtype=torch.long)
+        x_features = torch.tensor(features, dtype=torch.float32)
+        with torch.no_grad():
+            prediction = self.model(x_idx.to(self.device), x_features.to(self.device))
+        signal = torch.argmax(prediction).item()
+        return signal
+
+    async def preprocess(self, data):
+        print(f"Received data for {data.symbol}: open={data.open}, high={data.high}, low={data.low}, close={data.close}, volume={data.volume}")
+        ohlcv = [data.open, data.high, data.low, data.close, data.volume]
         
+        if self.prev_close is not None:
+            norm_open = log(ohlcv[0] / ohlcv[3])  # Normalize by close price
+            norm_high = log(ohlcv[1] / ohlcv[3]) # Normalize by close price
+            norm_low = log(ohlcv[2] / ohlcv[3]) # Normalize by close price
+            norm_volume = log(ohlcv[4])
+            momentum = log(ohlcv[3] / self.prev_close) # Normalize by close price
+            features = [norm_open, norm_high, norm_low, norm_volume, momentum]
+            signal = self.signal_generator(data.symbol, features)
+            print(f"Signal for {data.symbol}: {signal}")
+        self.prev_close = ohlcv[3]  # Update previous close price
+            
+
+
+if __name__ == "__main__":
+    trader = Trader()
+
+    trader.stream_data()
--- src/TrainModel.py
+++ src/TrainModel.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import torch
+import json
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader, random_split
 
@@ -8,12 +9,12 @@ from pathlib import Path
 from StockModel import StockModel
 
 
-RELOAD = False
+RELOAD = True
 
 PATH_TO_DATASETS = Path(r"datasets\5_us_txt\data\5 min\us")
 PATH_TO_MODELS = Path(r"models")
 PATH_TO_PRECOMPUTE = Path(r"precompute_cache")
-MODEL_NAME = Path(r"model8.pth")
+MODEL_NAME = Path(r"model1_weights.pth")
 
 PATH_TO_PRECOMPUTE.mkdir(parents=True, exist_ok=True)
 PATH_TO_MODELS.mkdir(parents=True, exist_ok=True)
@@ -22,7 +23,7 @@ PATH_TO_DATASETS.mkdir(parents=True, exist_ok=True)
 print(f"Path to datasets: {PATH_TO_DATASETS}")
 
 # SUBDIRS = [r"nasdaq etfs", r"nasdaq stocks\1", r"nasdaq stocks\2", r"nasdaq stocks\3", r"nyse etfs\1", r"nyse etfs\2", r"nyse stocks\1", r"nyse stocks\2", r"nysemkt stocks"]
-SUBDIRS = [Path(r"nysemkt stocks"),Path(r"nyse stocks\1"), Path(r"nyse stocks\2"), Path(r"nasdaq stocks\1"), Path(r"nasdaq stocks\2"), Path(r"nasdaq stocks\3")]
+SUBDIRS = [r"nysemkt stocks",r"nyse stocks\1", r"nyse stocks\2", r"nasdaq stocks\1", r"nasdaq stocks\2", r"nasdaq stocks\3"]
 
 #hyperparams
 BATCH_SIZE = 512
@@ -39,7 +40,7 @@ EPOCHS = 10
 
 #%47.04  embedding_dims = 32, hidden_layers = [2048, 2048, 1024] model8
 
-X_FEATURE_COLUMNS = ["time_sin", "time_cos", "day_sin", "day_cos", "month_sin", "month_cos", "day_of_month_sin", "day_of_month_cos","log_volume", "return", "range", "rolling_std", "SMA_5_norm", "log_SMA_5_norm", "momentum", "abs_return"]
+X_FEATURE_COLUMNS = ["norm_open", "norm_high", "norm_low", "log_volume", "momentum"]
 
 # time normalization constants
 OPEN = 9*60 + 30
@@ -47,7 +48,6 @@ CLOSE = 16*60
 
 class TrainModel:
     def __init__(self):
-
         self.x_id_tensor = None
         self.x_tensor = None
         self.y_tensor = None
@@ -57,8 +57,8 @@ class TrainModel:
         self.optimizer = None
         self.dataframe = pd.DataFrame()
         self.model = None
+        self.optim_model = None
         
-
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         print(f"Using device: {self.device}")
         self.load_tensors()
@@ -68,7 +68,6 @@ class TrainModel:
             self.preprocess_data()
             print("next")
             self.format_tensors(save=True)
-            
         print("next")
         self.prep_loaders()
         print("model ready to train.")
@@ -78,18 +77,20 @@ class TrainModel:
         df_list = []
         total = len(SUBDIRS)
         counter = 1
+        if (PATH_TO_DATASETS/r"datasets.parquet").exists():
+            self.dataframe = pd.read_parquet(PATH_TO_DATASETS/r"datasets.parquet")
+            return
+        
         if not PATH_TO_DATASETS.exists() or total < 1:
             print("dataset not found")
             return
-        
         for subdir in SUBDIRS:
-            subdir_path = Path(PATH_TO_DATASETS,subdir) #join dataset path object and subdir path object
+            subdir_path = PATH_TO_DATASETS/subdir #join dataset path object and subdir path object
             if not subdir_path.exists():
                 print("subdir not found")
                 return 
             print(f"loading {subdir_path}; Set: {counter}/{total}")
             counter+=1
-
             for file_path in subdir_path.glob("*.txt"):
                 if file_path.stat().st_size:
                     df = pd.read_csv(file_path,sep=',', engine="pyarrow")
@@ -97,97 +98,37 @@ class TrainModel:
         if not len(df_list) > 0:
             print("no csv found or loaded")
             return
-        
         self.dataframe = pd.concat(df_list, ignore_index=True)# basically, since the data is split into multiple files, we read each file and concatenate all the separate dataframes into a single dataframe ignoring their local indexes in the files.
         self.dataframe.dropna(inplace=True)
         self.dataframe.sort_values(by=["<TICKER>", "<DATE>", "<TIME>"], inplace=True) #sort it so everything is in order first by ticker, then date, then time
+
+        if not (PATH_TO_DATASETS/r"datasets.parquet").exists():
+            self.dataframe.to_parquet(PATH_TO_DATASETS/r"datasets.parquet", index=False) #save the concatenated dataframe as a parquet file for faster loading later
         print("load done.")
 
-
-    
 
     def preprocess_data(self):
         if self.dataframe is None:
             print("no dataframe found")
             return
         print("Preprocessing data...")
-
         self.dataframe['<TICKER>'] = self.dataframe['<TICKER>'].astype('category')
-        groups = self.dataframe.groupby('<TICKER>', sort=False)
+        if not (PATH_TO_PRECOMPUTE/r"embedding_lookup.json").exists():
+            embedding_lookup = dict(enumerate(self.dataframe['<TICKER>'].cat.categories))
+            with open(PATH_TO_PRECOMPUTE/r"embedding_lookup.json", "w") as f:
+                json.dump(embedding_lookup, f)
+            print("embedding lookup saved.")
 
-        #OHLCV log representation
-        self.dataframe["log_return"] = np.log(groups["<CLOSE>"]/groups["<OPEN>"]) #intrabar
-        self.dataframe["momentum"] = np.log(groups["<CLOSE>"]/groups["<CLOSE>"].shift(1)) #interbar
-        self.dataframe["log_HL"] = np.log(groups["<HIGH>"]/groups["<LOW>"]) #interbar
-        self.dataframe["z_score"] = (groups["<VOL>"]-groups['<VOL>'].mean())/(groups["<VOL>"].std(ddof=0)) #zscore
-
-        #technical indicators
-        self.dataframe["EMA_9"] = groups["<CLOSE>"].ewm(span=9, adjust=False).mean()
-        self.dataframe["EMA_21"] = groups["<CLOSE>"].ewm(span=21, adjust=False).mean()
-
-        self.dataframe['VWAP'] = ((((groups['<HIGH>'] + groups['<LOW>'] + groups['<CLOSE>']) / 3) *groups["<VOL>"]).cumsum())/(groups["<VOL>"].cumsum())
-        groups = self.dataframe.groupby('<TICKER>', sort=False)
-        self.dataframe['VWAP_distance'] = np.log(groups["<CLOSE>"]/groups["VWAP"])
-        #using span 9 or alpha 1/9 depending on wilders 
-        delta = groups['<CLOSE>'].diff()
-        avg_gain = delta.clip(lower=0).groupby('<TICKER>', sort=False).ewm(alpha=1/9, min_periods=9).mean()
-        avg_loss = -delta.clip(upper=0).groupby('<TICKER>', sort=False).ewm(alpha=1/9, min_periods=9).mean()
-        self.dataframe["RSI"] = 100-(100/(1+(avg_gain/avg_loss)))
-        
-        true_range = pd.concat([(groups["<HIGH>"] - groups["<LOW>"]),((groups["<HIGH>"]-groups["<CLOSE>"].shift(1)).abs()),((groups["<LOW>"]-groups["<CLOSE>"].shift(1)).abs())], axis = 1).max(axis = 1)
-        self.dataframe["ATR"]= true_range.groupby('<TICKER>', sort=False).ewm(alpha=1/9, adjust=False, min_periods=9).mean()
-
-        self.dataframe["log_volume"] = np.log(self.dataframe["<VOL>"] + 1)
-        self.dataframe["log_SMA_5_norm"] = self.dataframe["log_volume"] - self.dataframe.groupby("<TICKER>")["log_volume"].transform(lambda x: x.rolling(5, min_periods=1).mean())
-        
-
-        #normalize time to EST
-        dt_int = self.dataframe["<DATE>"].to_numpy(dtype="int64") * 1000000 + \
-        self.dataframe["<TIME>"].to_numpy(dtype="int64")
-        cet = pd.to_datetime(dt_int, format="%Y%m%d%H%M%S").tz_localize("Europe/Berlin")
-        est = cet.tz_convert("America/New_York")
-        minutes_total = est.hour * 60 + est.minute
-        t_norm = (minutes_total - OPEN) / (CLOSE - OPEN)
-
-        #cyclical time features
-        self.dataframe["time_sin"] = np.sin(2*np.pi*t_norm)
-        self.dataframe["time_cos"] = np.cos(2*np.pi*t_norm)
-        day_of_week = est.dayofweek
-        self.dataframe["day_sin"] = np.sin(2*np.pi*day_of_week/7)
-        self.dataframe["day_cos"] = np.cos(2*np.pi*day_of_week/7)
-        month_of_year = est.month
-        self.dataframe["month_sin"] = np.sin(2*np.pi*month_of_year/12)
-        self.dataframe["month_cos"] = np.cos(2*np.pi*month_of_year/12)
-        day_of_month = est.day
-        self.dataframe["day_of_month_sin"] = np.sin(2*np.pi*day_of_month/31)
-        self.dataframe["day_of_month_cos"] = np.cos(2*np.pi*day_of_month/31)
-
-        # #binary flags; ie. earnings reports, midday, start or end of trading day
-        # self.dataframe["start_of_quarter"] = month_of_year.isin([1,4,7,10]).astype(int)
-        # self.dataframe["end_of_quarter"] = month_of_year.isin([3,6,9,12]).astype(int)
-        # self.dataframe["start_of_year"] = (month_of_year == 1).astype(int)
-        # self.dataframe["end_of_year"] = (month_of_year == 12).astype(int)
-        # self.dataframe["quarter_of_year"] = (month_of_year-1)//3 + 1
-
-        
-        
-        #non intra-day needs groupby
-        
-        
-        
-        self.dataframe["abs_return"] = np.abs(self.dataframe["<CLOSE>"] - self.dataframe["<OPEN>"]) / self.dataframe["<OPEN>"]
-
-        self.dataframe["rolling_std"] = self.dataframe.groupby("<TICKER>")["return"].transform(lambda x: x.rolling(window=12, min_periods=1).std())
-        self.dataframe['SMA_5_norm'] = (self.dataframe.groupby('<TICKER>')['<CLOSE>'].transform(lambda x: x.rolling(5).mean())-self.dataframe["<CLOSE>"])/self.dataframe["<CLOSE>"]
-  
-
+        self.dataframe["norm_open"]=np.log(self.dataframe["<OPEN>"]/self.dataframe["<CLOSE>"])
+        self.dataframe["norm_high"]=np.log(self.dataframe["<HIGH>"]/self.dataframe["<CLOSE>"])
+        self.dataframe["norm_low"]=np.log(self.dataframe["<LOW>"]/self.dataframe["<CLOSE>"])
+        self.dataframe["log_volume"]=np.log(self.dataframe["<VOL>"])
+        self.dataframe["momentum"] = np.log(self.dataframe["<CLOSE>"]/self.dataframe.groupby('<TICKER>', sort=False, observed=False)["<CLOSE>"].shift(1)) #interbar momentum, basically the return from the previous close to the current close. this is what we will be trying to predict the direction of, so it's not included in the features.
         #ticker generation
         self.dataframe["ticker_id"] = self.dataframe["<TICKER>"].cat.codes
-        
         #label generation
-        future_return = self.dataframe.groupby("<TICKER>")["momentum"].transform(lambda x: x.shift(-1)) #if the previous close was lower than the current close, the PREVIOUS entry was a buy.
-        k=0.25
-        thresh = self.dataframe["rolling_std"]*k
+        future_return = self.dataframe.groupby("<TICKER>")["momentum"].shift(-1) #shift the momentum; if the previous close was lower than the current close, the PREVIOUS entry was a buy.
+        thresh = 0.001 #threshold for buy/sell signals, can be tuned as a hyperparameter. this means we only want to buy if the momentum is greater than 0.1% and sell if it's less than -0.1%, otherwise hold.
         self.dataframe["label"] = np.select(
             [future_return > thresh, future_return < -thresh],
             [0, 1],
@@ -195,9 +136,7 @@ class TrainModel:
         ) #buy / sell / hold, 0, 1, 2 
         self.dataframe.dropna(inplace=True)
         self.dataframe = self.dataframe[["ticker_id"]+X_FEATURE_COLUMNS+["label"]] #we just pass all the relevant fields (ticker id, features, and labels) in 1 dataframe to parse later in format_tensors
-        
         print("preprocess done.")
-
 
 
     def format_tensors(self, save = False): #does all the converting dataframe to tensor stuff. the rest of the values like feature size and embed size is in preprocess
@@ -215,18 +154,15 @@ class TrainModel:
 
         x_features = self.dataframe[X_FEATURE_COLUMNS].to_numpy(dtype=np.float32)
         self.x_tensor = torch.from_numpy(x_features).to(torch.float32)  
-
         if save:
             torch.save(self.x_tensor, Path(PATH_TO_PRECOMPUTE, r"x_tensor.pt"))
             torch.save(self.x_id_tensor, Path(PATH_TO_PRECOMPUTE,r"x_id_tensor.pt"))
             torch.save(self.y_tensor, Path(PATH_TO_PRECOMPUTE, r"y_tensor.pt"))
             print("saving formatted tensors.")
-
         print("formatting tensors done.")
 
 
     def load_tensors(self):
-        
         if not(Path(PATH_TO_PRECOMPUTE / r"y_tensor.pt").exists() and Path(PATH_TO_PRECOMPUTE/ "x_tensor.pt").exists() and Path(PATH_TO_PRECOMPUTE , r"x_id_tensor.pt").exists()):
             print("no saved tensors found")
             return
@@ -235,16 +171,16 @@ class TrainModel:
         self.x_id_tensor = torch.load(PATH_TO_PRECOMPUTE / r"x_id_tensor.pt")
         print("saved tensors loaded successfully.")
 
+
     def prep_loaders(self):
         if self.x_tensor is None or self.y_tensor is None or self.x_id_tensor is None:
             print("Tensors are not properly initialized. Cannot train model.")
             return
-        
         print("Preparing data loaders and model...")
         embed_size = self.x_id_tensor.max().item() + 1
         feature_size = len(X_FEATURE_COLUMNS)#number of features/inputs
         self.model = StockModel(feature_size=feature_size, embed_size=embed_size).to(self.device)
-        self.model = torch.compile(self.model)
+        self.optim_model = torch.compile(self.model)
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = torch.optim.AdamW(self.model.parameters(), lr = 1e-3)
         dataset = TensorDataset(self.x_id_tensor, self.x_tensor, self.y_tensor)
@@ -257,11 +193,11 @@ class TrainModel:
 
 
     def training_loop(self):
-        if self.criterion is None or self.optimizer is None or self.trainloader is None or self.model is None:
+        if self.criterion is None or self.optimizer is None or self.trainloader is None or self.optim_model is None:
             print("Data loaders or model not properly initialized. Cannot train model.")
             return
         print("training model...")
-        self.model.train()
+        self.optim_model.train()
         for epoch in range(EPOCHS):
             total_loss = 0.0
             counter = 1
@@ -270,24 +206,23 @@ class TrainModel:
                 x_batch = x_batch.to(self.device,non_blocking=True)
                 y_batch = y_batch.to(self.device,non_blocking=True)
                 self.optimizer.zero_grad()
-                outputs = self.model(x_id_batch, x_batch)
+                outputs = self.optim_model(x_id_batch, x_batch)
                 loss = self.criterion(outputs, y_batch)
                 loss.backward()
                 self.optimizer.step()
-
                 total_loss+=loss.item()
                 counter+=1
             print(f"loss for epoch {epoch} / {EPOCHS}: {(total_loss/counter):.4f}")
-
         torch.save(self.model.state_dict(), PATH_TO_MODELS / MODEL_NAME)
         print("training complete. Model saved.")
 
+
     def evaluate(self):
-        if self.testloader is None:
+        if self.testloader is None or self.optim_model is None:
             print("Test loader not properly initialized. Cannot evaluate model.")
             return
         print("testing model...")
-        self.model.eval()
+        self.optim_model.eval()
         correct = 0
         total = 0
         with torch.no_grad():
@@ -295,7 +230,7 @@ class TrainModel:
                 x_id_batch = x_id_batch.to(self.device, non_blocking=True)
                 x_batch = x_batch.to(self.device, non_blocking=True)
                 y_batch = y_batch.to(self.device, non_blocking=True)
-                outputs = self.model(x_id_batch, x_batch) # forward pass
+                outputs = self.optim_model(x_id_batch, x_batch) # forward pass
                 predicted = torch.argmax(outputs, dim=1) #predicted class labels
                 correct += (predicted == y_batch).sum().item() # count correct predictions
                 total += y_batch.size(0) # total number of labels
@@ -316,9 +251,8 @@ class TrainModel:
 if __name__ == "__main__":
 
     stock_model = TrainModel()
-    
-    stock_model.training_loop()
+    # stock_model.training_loop()
     # stock_model.load_model()
-    stock_model.evaluate()
+    # stock_model.evaluate()
     
 
