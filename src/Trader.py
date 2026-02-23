a415d9f97691ac1fb460acaa43ca6bf185682d6f
funnymonke0 <aivectorec@gmail.com>
2026-02-22 19:33:41 -0500

    made the signals actually good

--- src/Trader.py
+++ src/Trader.py
@@ -8,7 +8,8 @@ from StockModel import StockModel
 
 from alpaca.data.live import StockDataStream, CryptoDataStream
 from alpaca.trading.client import TradingClient
-from alpaca.trading.requests import LimitOrderRequest, MarketOrderRequest
+from alpaca.trading.requests import LimitOrderRequest, MarketOrderRequest, GetAssetsRequest
+from alpaca.trading.enums import AssetClass
 from alpaca.trading.enums import OrderSide, TimeInForce
 
 EPSILON = 2**-52 # the most tiniest didyblud
@@ -26,20 +27,18 @@ X_TENSOR = PATH_TO_PRECOMPUTE/ (DATASET_NAME+r"_x_tensor.pt")
 X_ID_TENSOR = PATH_TO_PRECOMPUTE/ (DATASET_NAME+r"_x_id_tensor.pt")
 Y_TENSOR = PATH_TO_PRECOMPUTE/ (DATASET_NAME+r"_y_tensor.pt")
 
-TICKERS = ["BTC", "ETH", "BCH", "LTC", "UNI", "SOL", "AVAX", "XRP", "DOGE", "USDC", "USDT","BCH","AAVE", "DOT", "LINK", "CRV", "XTZ", "YFI"] # List of tickers to subscribe to
+# TICKERS = ["BTC", "ETH", "BCH", "LTC", "UNI", "SOL", "AVAX", "XRP", "DOGE", "USDC", "USDT","BCH","AAVE", "DOT", "LINK", "CRV", "XTZ", "YFI"] # List of tickers to subscribe to
 X_FEATURE_COLUMNS = ["norm_open", "norm_high", "norm_low", "log_volume", "momentum"]
-
-
+TICKERS = []
 class Trader():
     def __init__(self):
-        self.bid_ask_map = {ticker+"/USD":[-1,-1] for ticker in TICKERS}
-        
+        self.tickers = TICKERS
         self.embedding_map = {}
         self.bars_window = pd.DataFrame() # Initialize an empty DataFrame to store incoming bars data
         with open(EMBEDDING_LOOKUP, 'r') as f:
             self.embedding_map = json.load(f)
             self.embedding_map = {v:int(k) for k,v in self.embedding_map.items()}# Invert the embedding map to get a mapping from ticker symbols to their corresponding IDs
-            print(f"Loaded embedding map: {self.embedding_map}")
+        print(self.embedding_map)
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         x_id_tensor = torch.load(X_ID_TENSOR)
         embed_size = x_id_tensor.max().item() + 1
@@ -58,9 +57,16 @@ class Trader():
         self.client = TradingClient(API_KEY, SECRET_KEY, paper=True)
         self.account = self.client.get_account()
         self.cached_buying_power = float(self.account.buying_power)
-        asset_map = {ticker:self.client.get_asset(ticker+"/USD") for ticker in TICKERS}
-        self.tradable = [ticker+"/USD" if asset_map[ticker].tradable else "" for ticker in TICKERS]
-        self.frac = [ticker+"/USD" if asset_map[ticker].fractionable else "" for ticker in TICKERS]
+        all_assets = self.client.get_all_assets(GetAssetsRequest(asset_class=AssetClass.CRYPTO))
+        self.tradable = [asset.symbol for asset in all_assets if asset.tradable]
+        self.frac = [asset.symbol for asset in all_assets if asset.fractionable]
+        self.tickers = [ticker.replace("/USD", "") for ticker in self.tradable if ticker.replace("/USD", ".V") in self.embedding_map.keys()]
+        all_positions = self.client.get_all_positions()
+        self.bid_ask_map = {ticker+"/USD":[-1,-1] for ticker in self.tickers}
+        self.local_positions = {ticker:0.0 for ticker in self.tickers}
+        for p in all_positions:
+            self.local_positions[p.symbol.replace("USD", "/USD")] = float(p.qty)
+        print(self.local_positions)
         print(f"Initialized trader with account status: {self.account.crypto_status}")
 
     
@@ -68,10 +74,10 @@ class Trader():
         # Connect to the Alpaca data stream and subscribe to the desired tickers
         print("Connecting to Alpaca data stream...")
         
-        self.stream.subscribe_bars(self.handle_data, *[ticker+"/USD" for ticker in TICKERS])
-        print("Subscribed to tickers: ", TICKERS)
-        self.stream.subscribe_quotes(self.handle_quote, *[ticker+"/USD" for ticker in TICKERS])
-        print("Subscribed to quotes: ", TICKERS)
+        self.stream.subscribe_bars(self.handle_data, *[ticker+"/USD" for ticker in self.tickers])
+        print("Subscribed to tickers: ", self.tickers)
+        self.stream.subscribe_quotes(self.handle_quote, *[ticker+"/USD" for ticker in self.tickers])
+        print("Subscribed to quotes: ", self.tickers)
         self.stream.run()
 
 
@@ -84,8 +90,9 @@ class Trader():
         return signal # No signal for the first data point since we don't have a previous close price
     
     def process(self, data):
+        data.symbol = data.symbol.replace("/USD", ".V") # Remove the "/USD" suffix from the symbol to match the ticker format in the embedding map
         new_row = pd.DataFrame([data.__dict__])
-        self.bars_window = pd.concat([self.bars_window, new_row]).sort_values(['symbol', 'timestamp']).dropna(inplace=False).iloc[-(50*len(TICKERS)):] 
+        self.bars_window = pd.concat([self.bars_window, new_row]).sort_values(['symbol', 'timestamp']).dropna(inplace=False).iloc[-(50*len(self.tickers)):] 
         self.bars_window['symbol'] = self.bars_window['symbol'].astype('category')
         if data.symbol not in self.bars_window['symbol'].cat.categories:
             print(f"Symbol {data.symbol} not in bars window categories yet. Skipping processing for this data point.")
@@ -110,24 +117,26 @@ class Trader():
     
     async def handle_quote(self, data):
         self.bid_ask_map[data.symbol] = [data.bid_price, data.ask_price]
-        print(f"quotes recieved {data.symbol}, bid: {data.bid_price}, ask: {data.ask_price}")
+        # print(f"quotes recieved {data.symbol}, bid: {data.bid_price}, ask: {data.ask_price}")
         # print(self.bid_ask_map)
 
     async def handle_data(self, data):
         print(f"data received: {data}")
-        data.symbol = data.symbol.replace("/USD", ".V") # Remove the "/USD" suffix from the symbol to match the ticker format in the embedding map
+        symbol = data.symbol
         ticker_id, features = self.process(data)
         if ticker_id is not None and features is not None:
             # print(f"Ticker ID: {ticker_id}, Features: {features}")
             signal = self.signal_generator(ticker_id=ticker_id, features=features)
             print(f"Generated signal: {signal} for data {data}")
             with open(PATH_TO_PRECOMPUTE / "signal_log.txt", "a") as f:
-                f.write(f"{data.timestamp}: {data.symbol} - Signal: {signal}\n")
-            order = self.portfolio_management(signal, data.symbol)
+                f.write(f"{data.timestamp}: {symbol} - Signal: {signal}\n")
+            order = self.portfolio_management(signal, symbol)
             if order is not None:
                 try:
                     self.client.submit_order(order)
                     print(f"Order submitted: {order}")
+                    self.cached_buying_power = self.account.buying_power
+                    self.local_positions[symbol] = self.client.get_open_position(symbol).qty
                 except Exception as e:
                     print(f"Error submitting order: {e}")
         else:
@@ -135,20 +144,18 @@ class Trader():
             pass
 
     def portfolio_management(self, signal, symbol):
-
-        symbol = symbol.replace(".V", "/USD") # Convert symbol to match Alpaca's format for crypto trading
-
-
+        print(symbol)
         if symbol not in self.tradable:
             print(f"Asset {symbol} is not tradable.")
             return
         ask, bid = self.bid_ask_map[symbol]
-        if ask == -1 or bid ==-1:
+        if ask <=0 or bid <=0:
             print(f"need to get quote data for {symbol}")
             return
-        buying_power = self.cached_buying_power*0.99
+        buying_power = float(self.cached_buying_power)*0.95
         limit = 0.05 * buying_power # per trade limit
         direction = signal[0]-signal[1]
+        print(direction)
         order = None
         if direction > 0: # Buy signal (limit order placed slightly above current price)
             limit_price = ask * (1 + 0.001)
@@ -163,18 +170,18 @@ class Trader():
                     time_in_force=TimeInForce.GTC # Cancel if not filled by end of day
                 )
         elif direction < 0: # Sell signal
-            
-            limit_price = bid * (1 - 0.001)
-            quantity = min(int(limit*abs(direction) // limit_price), int(buying_power//limit_price)) if symbol not in self.frac else min(limit*abs(direction)/limit_price, buying_power/limit_price)
-            quantity = min(self.client.get_open_position(symbol).qty, quantity)
-            print(f"Placing limit sell order for {symbol} at price {limit_price} with quantity {quantity}")
-            order = LimitOrderRequest(
-                symbol=symbol,
-                limit_price = limit_price,
-                qty=quantity,
-                side=OrderSide.SELL,
-                time_in_force=TimeInForce.GTC # Cancel if not filled by end of day
-            )
+            if symbol in self.local_positions.keys():
+                limit_price = bid * (1 - 0.001)
+                quantity = min(int(limit*abs(direction) // limit_price), int(buying_power//limit_price)) if symbol not in self.frac else min(limit*abs(direction)/limit_price, buying_power/limit_price)
+                quantity = min(self.local_positions[symbol], quantity)
+                print(f"Placing limit sell order for {symbol} at price {limit_price} with quantity {quantity}")
+                order = LimitOrderRequest(
+                    symbol=symbol,
+                    limit_price = limit_price,
+                    qty=quantity,
+                    side=OrderSide.SELL,
+                    time_in_force=TimeInForce.GTC # Cancel if not filled by end of day
+                )
 
         return order
             
